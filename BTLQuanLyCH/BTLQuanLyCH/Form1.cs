53a311d9e73b6a951781b3acded19b6a4818384d
chien67 <chien672001@gmail.com>
2022-05-16 14:08:52 +0700

    1
    Form Main và Login

---
+++ BTLQuanLyCH/BTLQuanLyCH/Form1.cs
@@ -0,0 +1,14 @@
+namespace BTLQuanLyCH
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        private void btnNhanVien_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BTLQuanLyCH/BTLQuanLyCH/frmDangNhap.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace BTLQuanLyCH
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.RadioButton();
+            this.btnNo = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(130, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tài khoản ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(28, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "UserName :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(28, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 34);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password   :";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(192, 204);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(230, 27);
+            this.txtUserName.TabIndex = 4;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(192, 272);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(230, 27);
+            this.txtPass.TabIndex = 5;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDangNhap.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDangNhap.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDangNhap.Location = new System.Drawing.Point(28, 377);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(394, 46);
+            this.btnDangNhap.TabIndex = 6;
+            this.btnDangNhap.Text = "Login";
+            this.btnDangNhap.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(28, 319);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 34);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Admin ";
+            // 
+            // btnYes
+            // 
+            this.btnYes.AutoSize = true;
+            this.btnYes.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnYes.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnYes.Location = new System.Drawing.Point(171, 315);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(78, 38);
+            this.btnYes.TabIndex = 8;
+            this.btnYes.TabStop = true;
+            this.btnYes.Text = "Yes";
+            this.btnYes.UseVisualStyleBackColor = true;
+            // 
+            // btnNo
+            // 
+            this.btnNo.AutoSize = true;
+            this.btnNo.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnNo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnNo.Location = new System.Drawing.Point(294, 315);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(73, 38);
+            this.btnNo.TabIndex = 9;
+            this.btnNo.TabStop = true;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(447, 486);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDangNhap";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtUserName;
+        private TextBox txtPass;
+        private Button btnDangNhap;
+        private Label label4;
+        private RadioButton btnYes;
+        private RadioButton btnNo;
+    }
+}
