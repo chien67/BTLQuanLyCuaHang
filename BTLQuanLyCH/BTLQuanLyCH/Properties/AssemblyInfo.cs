a7e069ecbbd38613c341984e535da33edf276a0d
duckiman <draballzkai@gmail.com>
2022-05-23 22:23:19 +0700

    update

--- BTLQuanLyCH/BTLQuanLyCH/Properties/AssemblyInfo.cs
+++ BTLQuanLyCH/BTLQuanLyCH/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BTLQuanLyCH")]
+[assembly: AssemblyTitle("GUI_QuanLyCH")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BTLQuanLyCH")]
+[assembly: AssemblyProduct("GUI_QuanLyCH")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- BTLQuanLyCH/BTLQuanLyCH/frmDangNhap.cs
+++ BTLQuanLyCH/BTLQuanLyCH/frmDangNhap.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BTLQuanLyCH
+namespace GUI_QuanLyCH
 {
     public partial class frmDangNhap : Form
     {
